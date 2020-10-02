379d0f261a5c79dd7ffc6d66ffb202a261e200ce
Pablo Benmaman <montenegro.pablo@outlook.com>
2020-10-02 20:44:33 +0200

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+const plugins = require('./gatsby-config.plugins');
+
+module.exports = {
+    pathPrefix: 'gatsby-material-ui-starter',
+    siteMetadata: {
+        title: 'Gatsby Material UI Starter',
+        description: 'Kick off your next, great Gatsby project with this Material UI starter.',
+        author: '@blopa',
+    },
+    plugins,
+};
